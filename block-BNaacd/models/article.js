3060abd8b275bb7f1f704bf847aa04acae03c274
artikgupta <artikgupta>
2021-04-22 17:35:01 +0530

    completed block-4 of schema types

---
+++ block-BNaacd/models/article.js
@@ -0,0 +1,15 @@
+
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name : String,
+    email:String,
+    age : Number
+})
+
+var userSchema= new Schema({
+    email:{type: String, lowercase:true},
+    age:{type:Number}
+})
